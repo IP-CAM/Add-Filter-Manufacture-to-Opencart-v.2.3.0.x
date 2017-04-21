e19e4b884bb52c23aa6acf914b74f2d060527ab2
arashgh68 <arashgh68@gmail.com>
2017-04-21 23:02:34 +0430

    Add files via upload

---
+++ Upload/admin/language/fa-ir/extension/module/filterpromahan.php
@@ -0,0 +1,18 @@
+<?php
+// admin/language/fa-ir/extension/module/filterpromahan.php
+// Heading
+$_['heading_title']    = 'فیلتر پیشرفته ماهان';
+ 
+// Text
+$_['text_module']      = 'ماژول ها';
+$_['text_success']     = 'شما ماژول پیشرفته را با موفقیت تغییر دادید';
+$_['text_edit']        = 'ویرایش ماژول';
+ 
+// Entry
+$_['entry_name']       = 'نام ماژول';
+$_['entry_limit']      = 'محدودیت';
+$_['entry_status']     = 'وضعیت';
+ 
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify Recent Products module!';
+$_['error_name']       = 'Module Name must be between 3 and 64 characters!';
