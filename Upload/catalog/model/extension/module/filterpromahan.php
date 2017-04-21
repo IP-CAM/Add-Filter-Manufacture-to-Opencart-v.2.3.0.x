49ba17d49214e0d14905d4aee2bfa5427f6667f5
arashgh68 <arashgh68@gmail.com>
2017-04-22 00:08:10 +0430

    Add getManufactureName method

--- Upload/catalog/model/extension/module/filterpromahan.php
+++ Upload/catalog/model/extension/module/filterpromahan.php
@@ -1,4 +1,14 @@
 <?php
 class ModelModuleFilterpromahan extends Model {
   
+  public function getManufactureName($category_id){
+		
+		$sql = "SELECT DISTINCT" . DB_PREFIX . "manufacturer.name FROM " . DB_PREFIX . "product LEFT JOIN " . DB_PREFIX . "product_to_category on " . DB_PREFIX . "product.product_id = " . DB_PREFIX . "product_to_category.product_id LEFT JOIN " . DB_PREFIX . "manufacturer on " . DB_PREFIX . "manufacturer.manufacturer_id = " . DB_PREFIX . "product.manufacturer_id WHERE " . DB_PREFIX . "product_to_category.category_id = ".$category_id;
+		
+		$query = $this->db->query($sql);
+		
+		$manufacturer_data = $query->rows;	
+
+	}
+  
 }
