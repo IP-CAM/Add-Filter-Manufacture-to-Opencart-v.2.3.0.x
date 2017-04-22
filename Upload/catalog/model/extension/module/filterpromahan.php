ce81f7cd80e6d0e545a4fdcbf06d32168a1e1816
arashgh68 <arashgh68@gmail.com>
2017-04-22 07:09:16 +0430

    add manufacture_id

--- Upload/catalog/model/extension/module/filterpromahan.php
+++ Upload/catalog/model/extension/module/filterpromahan.php
@@ -3,7 +3,7 @@ class ModelModuleFilterpromahan extends Model {
   
   public function getManufactureName($category_id){
 		
-		$sql = "SELECT DISTINCT" . DB_PREFIX . "manufacturer.name FROM " . DB_PREFIX . "product LEFT JOIN " . DB_PREFIX . "product_to_category on " . DB_PREFIX . "product.product_id = " . DB_PREFIX . "product_to_category.product_id LEFT JOIN " . DB_PREFIX . "manufacturer on " . DB_PREFIX . "manufacturer.manufacturer_id = " . DB_PREFIX . "product.manufacturer_id WHERE " . DB_PREFIX . "product_to_category.category_id = ".$category_id;
+		$sql = "SELECT DISTINCT" . DB_PREFIX . "manufacturer.name " . DB_PREFIX . "manufacturer.manufacturer_id FROM " . DB_PREFIX . "product LEFT JOIN " . DB_PREFIX . "product_to_category on " . DB_PREFIX . "product.product_id = " . DB_PREFIX . "product_to_category.product_id LEFT JOIN " . DB_PREFIX . "manufacturer on " . DB_PREFIX . "manufacturer.manufacturer_id = " . DB_PREFIX . "product.manufacturer_id WHERE " . DB_PREFIX . "product_to_category.category_id = ".$category_id;
 		
 		$query = $this->db->query($sql);
 		
