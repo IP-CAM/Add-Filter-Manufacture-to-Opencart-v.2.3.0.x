efe778a06aabb13ebd7342d5113898e3a87edaac
arashgh68 <arashgh68@gmail.com>
2017-04-22 07:04:53 +0430

    Update filterpromahan.php

--- Upload/catalog/controller/extension/module/filterpromahan.php
+++ Upload/catalog/controller/extension/module/filterpromahan.php
@@ -1,4 +1,61 @@
 <?php
 class ControllerModuleFilterpromahan extends Controller {
   
+  public function index() {
+		
+		$this->load->language('extension/module/magicfilter');
+        $data['heading_title'] = $this->language->get('heading_title');
+		
+		$this->load->model('extension/module/filterpromahan');
+		
+
+		
+		if (isset($this->request->get['filter'])) {
+            $filter = $this->request->get['filter'];
+        } else {
+            $filter = '';
+        }
+
+        if (isset($this->request->get['sort'])) {
+            $sort = $this->request->get['sort'];
+        } else {
+            $sort = 'p.sort_order';
+        }
+
+        if (isset($this->request->get['order'])) {
+            $order = $this->request->get['order'];
+        } else {
+            $order = 'ASC';
+        }
+
+        if (isset($this->request->get['page'])) {
+            $page = $this->request->get['page'];
+        } else {
+            $page = 1;
+        }
+
+        if (isset($this->request->get['limit'])) {
+            $limit = (int) $this->request->get['limit'];
+        } else {
+            $limit = $this->config->get($this->config->get('config_theme') . '_product_limit');
+        }
+        if (isset($this->request->get['path'])) {
+            if (stristr($this->request->get['path'], '_') === FALSE) {
+                $parts = $this->request->get['path'];
+                $category_id = $this->request->get['path'];
+            } else {
+
+                $path = '';
+                $parts = explode('_', (string) $this->request->get['path']);
+                $category_id = '';
+                $category_id = (int) array_pop($parts);
+            }
+			
+			$manufactureNames = $this->model_extension_module_filterpromahan->getManufactureName($category_id);
+
+			
+		}
+
+	}
+  
 }
